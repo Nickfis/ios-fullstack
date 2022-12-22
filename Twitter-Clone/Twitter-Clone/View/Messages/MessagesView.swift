f8d60f51a5523a93e7515238a53dc50c5644b6ac
Niklas <niklas@kizo-agency.com>
2022-12-22 12:19:54 +0100

    Message tab placeholders

--- Twitter-Clone/Twitter-Clone/View/Messages/MessagesView.swift
+++ Twitter-Clone/Twitter-Clone/View/Messages/MessagesView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct MessagesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ScrollView {
+                ForEach(0..<14) {_ in
+                    MessageCell()
+                }
+            }
+        }
     }
 }
 
