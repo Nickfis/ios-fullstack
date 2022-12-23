e49c613e9bd3a546668bb08ccfc9bc4ffa85c545
Niklas <niklas@kizo-agency.com>
2022-12-23 13:52:47 +0100

    Register View

---
+++ Twitter-Clone/Twitter-Clone/View/Authentication/RegisterView.swift
@@ -0,0 +1,74 @@
+//
+//  RegisterView.swift
+//  Twitter-Clone
+//
+//  Created by Niklas Fischer on 23/12/22.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                HStack {
+                    Button(action: {}, label: {
+                        Text("Cancel")
+                            .foregroundColor(.blue)
+                    })
+                    Spacer()
+                }
+                .padding(.horizontal)
+                
+                Image("Twitter")
+                    .resizable()
+                    .scaledToFill()
+                    .padding(.trailing)
+                    .frame(width: 20, height: 20)
+            }
+            
+            Text("Create your account")
+                .font(.title)
+                .bold()
+                .padding(.top, 35)
+            
+            VStack(alignment: .leading, spacing: nil) {
+                CustomAuthTextField(placeholder: "Name", text: $name)
+                CustomAuthTextField(placeholder: "Phone number or email", text: $email)
+                SecureAuthTextField(placeholder: "Password", text: $password)
+            }
+            
+            Spacer(minLength: 0)
+            
+            VStack {
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundColor(.gray)
+                
+                HStack {
+                    Spacer()
+                    Button(action: {}, label: {
+                        Capsule()
+                            .frame(width: 60, height: 30, alignment: .center)
+                            .foregroundColor(Color("background"))
+                            .overlay {
+                                Text("Next")
+                                    .foregroundColor(.white)
+                            }
+                    })
+                }.padding(.trailing)
+            }
+            
+        }
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
---
+++ Twitter-Clone/Twitter-Clone/View/Items/SecureAuthTextField.swift
@@ -0,0 +1,34 @@
+//
+//  CustomAuthTextField.swift
+//  Twitter-Clone
+//
+//  Created by Niklas Fischer on 23/12/22.
+//
+
+import SwiftUI
+
+struct SecureAuthTextField: View {
+    var placeholder: String
+    @Binding var text: String
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .leading) {
+                if text.isEmpty {
+                    Text(placeholder)
+                        .foregroundColor(.gray)
+                }
+                
+                SecureField("", text: $text)
+                    .frame(height: 45)
+                    .foregroundColor(Color("background"))
+            }
+            
+            Rectangle()
+                .frame(height: 1, alignment: .center)
+                .foregroundColor(.gray)
+                .padding(.top, -2)
+        }
+        .padding(.horizontal)
+    }
+}
