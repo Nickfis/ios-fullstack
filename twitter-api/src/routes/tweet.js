4fb2cc4014d91d7b04946d0ae7fc4b5ed5f8c504
Niklas <niklas@kizo-agency.com>
2022-12-30 18:42:18 +0100

    Tweet routes done

--- twitter-api/src/routes/tweet.js
+++ twitter-api/src/routes/tweet.js
@@ -76,4 +76,57 @@ router.get("/tweets/:id/image", async (req, res) => {
   }
 });
 
+// Like tweet function
+router.put("/tweets/:id/like", auth, async (req, res) => {
+  try {
+    const tweet = await Tweet.findById(req.params.id);
+    if (!tweet.likes.includes(req.user.id)) {
+      tweet.likes.push(req.user.id);
+      return res.status(200).send("Post has been liked");
+    } else {
+      res.status(403).json({ error: "You have already like this tweet" });
+    }
+  } catch (e) {
+    return res.status(500).send(e);
+  }
+});
+
+// Unlike tweet function
+router.put("/tweets/:id/unlike", auth, async (req, res) => {
+  try {
+    const tweet = await Tweet.findById(req.params.id);
+    if (tweet.likes.includes(req.user.id)) {
+      tweet.likes = tweet.likes.filter((userId) => userId !== req.user.id);
+      return res.status(200).send("Post has been unliked");
+    } else {
+      res.status(403).json({ error: "You have not liked this tweet before." });
+    }
+  } catch (e) {
+    return res.status(500).send(e);
+  }
+});
+
+// One specific tweet
+router.put("/tweets/:id", async (req, res) => {
+  try {
+    const tweet = await Tweet.findById(req.params.id);
+    if (!tweet) {
+      return res.status(404).send("Tweet can't be found");
+    }
+    return res.status(200).json({ tweet });
+  } catch (e) {
+    return res.status(500).send(e);
+  }
+});
+
+// One specific tweet
+router.put("/tweets-by-user/:id", async (req, res) => {
+  try {
+    const tweets = await Tweet.find({ user: req.params.id });
+    return res.status(200).send(tweets);
+  } catch (e) {
+    return res.status(500).send(e);
+  }
+});
+
 module.exports = router;
